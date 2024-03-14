6c420d54e2f06d9a68e70375a081e6e7fe3f1d90
Meet Vekaria <mvekaria@mobiquityinc.com>
2024-03-14 16:02:56 +0530

    Lecture - 06

--- src/componenets/Header.js
+++ src/componenets/Header.js
@@ -1,8 +1,16 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
+
+  // if no dependency array => useEffect is called on every render
+  // if dependency array is empty = [] => useEffect is called on initial render (Just once)
+  // if dependency array is [btnName] => called everytime btnName is updated.
+  // useEffect(() => {
+  //   console.log("useEffect called");
+  // }, [btnName]);
 
   return (
     <div className="header">
@@ -11,9 +19,15 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
           <li>Cart</li>
           <button
             className="login"
